3bfa538c4e5f0e08950434ca42288edb6171cdbf
austin <austinstevesk@gmail.com>
2021-12-30 23:17:21 +0300

    :fire: abstractuser details

--- localusers/models.py
+++ localusers/models.py
@@ -5,4 +5,8 @@ from django.db import models
 # Create your models here.
 class LocalUser(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(_("email_address"), max_length=254)
     first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    phone = models.PhoneNumberField(_("phone_number"))
+    
